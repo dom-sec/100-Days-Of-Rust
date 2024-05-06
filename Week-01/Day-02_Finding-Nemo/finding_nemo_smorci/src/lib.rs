4b4dff073b8e932f13f2dbc41647410d1e423661
Smorci <szekelyschmarton@gmail.com>
2024-05-06 18:41:19 +0100

    Remove iterator madness and match regex option

--- Week-01/Day-02_Finding-Nemo/finding_nemo_smorci/src/lib.rs
+++ Week-01/Day-02_Finding-Nemo/finding_nemo_smorci/src/lib.rs
@@ -1,91 +1,18 @@
-use std::io;
+//! This crate provides functions to find the whitespace separated word `Nemo` in a given sentence
+//!
 use regex::Regex;
 
-// Get user input
-pub fn get_input() -> io::Result<String> {
-    let mut buffer = String::new();
-    let stdin = io::stdin();
-    stdin.read_line(&mut buffer)?;
-    Ok(buffer)
-}
-
-// Find Nemo
-// Lumberjack solution
-// This solution is bad and error prone, finished it to learn iterators
-pub fn find_nemo_iterator(buffer: &str) {
-
-    let bytes = buffer.as_bytes();
-    let mut iter = bytes.iter().peekable();
-    let mut found: bool = false;
-    let mut index = 0_usize;
-
-    while iter.peek() != None {
-        if (index == 0_usize) || (iter.peek() == Some(&&b' ')) {
-            if iter.peek() == Some(&&b' ') { iter.next(); index += 1_usize; };
-            if iter.peek() == Some(&&b'N') {
-                iter.next();
-                if iter.peek() == Some(&&b'e') {
-                    iter.next();
-                    if iter.peek() == Some(&&b'm') {
-                        iter.next();
-                        if iter.peek() == Some(&&b'o') {
-                            iter.next();
-                            if iter.peek() == Some(&&b' ') {
-                                found = true;
-                                // Save string slice before nemo
-                                let slice: &str = &buffer[..index];
-                                // Split into iterator based on whitespaces
-                                let split = slice.split(' ');
-                                // Count number of words, next is nemo (we don't have to add one
-                                // because count consumes None at the end of the iterator
-                                let place_of_nemo: usize = split.count();
-
-                                println!("Found Nemo at {place_of_nemo}!");
-
-                                break;
-
-                            }
-                            index += 1_usize;
-                        }
-                        index += 1_usize;
-                    }
-                    index += 1_usize;
-                }
-                index += 1_usize;
-            }
-            iter.next();
-            index += 1_usize;
-        } else {
-            iter.next();
-            index += 1_usize;
-        }
-    }
-
-    if !found {
-        println!("I can't find Nemo :(");
-    }
-}
-
-// Find Nemo Regex Solution
-// TODO This solution panics, match the Error and resolve it
+/// Find Nemo Regex Solution
 pub fn find_nemo_regex (buffer: &str) {
-    let nemo_re = Regex::new(r"\s*Nemo\s+").expect("Wrong regex pattern.");
-    let nemo_match = nemo_re.find(buffer.trim()).expect("Error matching regex pattern on buffer.");
 
-
-    if !nemo_match.is_empty() {
-
-        let slice: &str = &buffer[..nemo_match.start()+1];
-
-        let split = slice.split(' ');
-
-        let place_of_nemo: usize = split.count();
-
-        println!("Found Nemo at {place_of_nemo}!");
-
-    } else {
-
-        println!("I can't find Nemo :(");
-
+    let nemo_regex = Regex::new(r"\s*Nemo\s+").expect("Wrong regex pattern.");
+    match nemo_regex.find(buffer.trim()) {
+        Some(value) => {
+            let slice: &str = &buffer[..value.start()+1];
+            let split = slice.split(' ');
+            let place_of_nemo: usize = split.count();
+            println!("Found Nemo at {place_of_nemo}!");
+        },
+        None => println!("I can't find Nemo :("),
     }
 }
--- Week-01/Day-02_Finding-Nemo/finding_nemo_smorci/src/main.rs
+++ Week-01/Day-02_Finding-Nemo/finding_nemo_smorci/src/main.rs
@@ -1,14 +1,17 @@
-use finding_nemo_smorci::{get_input, find_nemo_regex};
+use finding_nemo_smorci::{find_nemo_regex};
+use std::io;
 
 fn main() {
 
     println!("I will find Nemo in the given sentence.");
-    println!("Make sure to always sepparate punctuation from the last word. (ex. Here is Nemo !)");
+    println!("Make sure to always sepparate punctuation from the last word and awlays use punctuation at the end of the word. (ex. Here is Nemo !)");
     println!("Please enter a sentence");
 
-    let buffer: String = get_input().expect("Unable to read input from stdin.");
+    let mut buffer: String = String::new();
 
-    //find_nemo_iterator(&buffer);
+    io::stdin()
+        .read_line(&mut buffer)
+        .expect("Error reading input from stdin.");
     
     find_nemo_regex(&buffer);
 
