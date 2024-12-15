69e2198d49feab11878f2ccce055fb3d1bcfe576
katerichter <katerichter5@gmail.com>
2024-12-14 21:24:35 -0800

    remove debug

--- Week-01/Day-01_Convert-Ages-To-Days/challenge1/target/debug/deps/challenge1-32f20e78b592c1cc.d
+++
@@ -1,5 +0,0 @@
-/Users/kmitchell/Desktop/rust/100-Days-Of-Rust/Week-01/Day-01_Convert-Ages-To-Days/challenge1/target/debug/deps/libchallenge1-32f20e78b592c1cc.rmeta: src/main.rs
-
-/Users/kmitchell/Desktop/rust/100-Days-Of-Rust/Week-01/Day-01_Convert-Ages-To-Days/challenge1/target/debug/deps/challenge1-32f20e78b592c1cc.d: src/main.rs
-
-src/main.rs:
--- Week-01/Day-01_Convert-Ages-To-Days/challenge1/target/debug/deps/challenge1-331fd76d0c47ffd7.d
+++
@@ -1,5 +0,0 @@
-/Users/kmitchell/Desktop/rust/100-Days-Of-Rust/Week-01/Day-01_Convert-Ages-To-Days/challenge1/target/debug/deps/libchallenge1-331fd76d0c47ffd7.rmeta: src/main.rs
-
-/Users/kmitchell/Desktop/rust/100-Days-Of-Rust/Week-01/Day-01_Convert-Ages-To-Days/challenge1/target/debug/deps/challenge1-331fd76d0c47ffd7.d: src/main.rs
-
-src/main.rs:
