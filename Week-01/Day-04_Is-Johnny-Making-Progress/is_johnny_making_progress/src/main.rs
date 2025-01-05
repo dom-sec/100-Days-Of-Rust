99eb05ecced98351d0da6627063d70dc3b9a04cf
enriqueb721 <eabg97@gmail.com>
2025-01-04 23:26:10 -0500

    finish day 4

--- Week-01/Day-04_Is-Johnny-Making-Progress/is_johnny_making_progress/src/main.rs
+++ Week-01/Day-04_Is-Johnny-Making-Progress/is_johnny_making_progress/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        let n: i32 = input.trim().parse().unwrap();
+        if n == 0 {
+            break;
+        }
+        let mut prev = 1e9 as i32;
+        let mut progress_days = 0;
+        for _ in 0..n {
+            let mut input = String::new();
+            std::io::stdin().read_line(&mut input).unwrap();
+            let current: i32 = input.trim().parse().unwrap();
+            if current > prev {
+                progress_days += 1;
+            }
+            prev = current
+        }
+        println!("Progress days: {progress_days}");
+    }
 }
