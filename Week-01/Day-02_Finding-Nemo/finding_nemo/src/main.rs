ad76b364e9421c09a5e47c5754b4b6e795fada63
enriqueb721 <eabg97@gmail.com>
2025-01-04 21:55:37 -0500

    finish day 2

--- Week-01/Day-02_Finding-Nemo/finding_nemo/src/main.rs
+++ Week-01/Day-02_Finding-Nemo/finding_nemo/src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        println!("Enter a sentence to find Nemo: ");
+        let mut nemo_finding = String::new();
+
+        io::stdin()
+            .read_line(&mut nemo_finding)
+            .expect("Could not read the sentence. You have to use a string instead");
+
+        let words: Vec<&str> = nemo_finding.split_whitespace().collect();
+        let mut found = false;
+        for (i, &word) in words.iter().enumerate() {
+            if word == "Nemo" {
+                println!("I found Nemo at {}", i + 1);
+                found = true;
+                break;
+            }
+        }
+        if !found {
+            println!("I can't find Nemo");
+        }
+    }
 }
