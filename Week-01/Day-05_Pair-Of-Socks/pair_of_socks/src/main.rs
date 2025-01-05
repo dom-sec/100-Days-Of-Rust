9d9d4a4d4df911310d4074c1e20f2a3761a9558e
enriqueb721 <eabg97@gmail.com>
2025-01-04 23:50:05 -0500

    finish day 5

---
+++ Week-01/Day-05_Pair-Of-Socks/pair_of_socks/src/main.rs
@@ -0,0 +1,19 @@
+use std::collections::HashMap;
+
+fn main() {
+    loop {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+    
+        let mut socks: HashMap<char, u32> = HashMap::new();
+        for i in input.chars() {
+            socks.entry(i).and_modify(|e| *e += 1).or_insert(1);
+        }
+        let mut pair_socks = 0;
+        for (_, value) in socks.iter() {
+            // println!("{}: {}", key, value);
+            pair_socks += value / 2;
+        }
+        println!("{}", pair_socks);
+    }
+}
