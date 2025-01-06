2f36752dbcc172b5515685be57e2f2596cf7d248
enriqueb721 <eabg97@gmail.com>
2025-01-06 00:59:05 -0500

    finish day 6

--- Week-01/Day-06_Next-Prime/next_prime/src/main.rs
+++ Week-01/Day-06_Next-Prime/next_prime/src/main.rs
@@ -1,3 +1,24 @@
+fn is_prime(n: i32) -> bool {
+    for i in 2..(n as f64).sqrt() as i32 + 1 {
+        if n % i == 0 {
+            return false;
+        }
+    }
+    true
+}
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+    
+        let mut n: i32 = input.trim().parse().unwrap();
+        loop {
+            if is_prime(n) {
+                println!("{}", n);
+                break;
+            }
+            n += 1;
+        }
+    }
 }
